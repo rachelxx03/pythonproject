726900562aef1090eaebded644fa9685e68c2836
rachelxx03 <53119641+rachelxx03@users.noreply.github.com>
2022-02-19 17:35:46 -0400

    Add files via upload

---
+++ testjs.js
@@ -0,0 +1,8 @@
+console.log("hello this is the first time i use javescript");
+let input=parseInt(prompt("try a number"));
+if (input%2===0){
+    console.log("it is odd number");
+}
+else{
+    console.log("it is even");
+}
